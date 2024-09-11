86e61f6f86f8f5d44697d488e3837483980d508a
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-09-11 12:54:55 +0900

    ✨ fetchin grl

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import requests
 from lxml import html
 
-# ウェブページからHTMLデータを取得
-year = 2024
-url = 'https://openaccess.thecvf.com/CVPR' + str(year) + '?day=all'
-response = requests.get(url)
-html_content = response.text
+
+def main():
+    # ウェブページからHTMLデータを取得
+    year = 2024
+    url = 'https://openaccess.thecvf.com/CVPR' + str(year) + '?day=all'
+    response = requests.get(url)
+    html_content = response.text
 
 # lxmlを使用してHTMLをパース
-tree = html.fromstring(html_content)
+    tree = html.fromstring(html_content)
 
 # XPathを使用して要素を取得
-elements = tree.xpath('/html/body/div[3]/dl/dt')
+    elements = tree.xpath('/html/body/div[3]/dl/dt')
 
 # 取得した要素を表示
-for element in elements:
-    print(element.text_content())
+    for element in elements:
+        a_tag = element.find('a')
+        if a_tag is not None:
+            title = a_tag.text_content().strip()
+            href = a_tag.get('href')
+            if href and not href.startswith('http'):
+                href = 'https://openaccess.thecvf.com' + href
+            print(title + ',' + href)
+
+
+if __name__ == "__main__":
+    main()
